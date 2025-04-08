a3beb49d51fcf2443934657f7da9540ea856c7c5
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-04-08 15:43:41 +0600

    second commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
