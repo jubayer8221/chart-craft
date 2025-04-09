04573742b8708746468907a1a98ce13c8f0c61a2
Jubayer Al Mahmud <jubayer8221@gmail.com@users.noreply.github.com>
2025-04-10 00:49:27 +0600

    solve error

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import BuySellChart from "@/components/buySellChart/BuySellChart";
+import React from "react";
 
 const MainPage = () => {
   return (
     <>
       <span className="font-bold text-4xl">Home</span>
-      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-    </>
-  )
-}
 
-export default MainPage
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      {/* <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div> */}
+      {/* 
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div> */}
+      <div className="border-dashed border border-zinc-500 w-full h-full rounded-lg">
+        <BuySellChart></BuySellChart>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
