8a7ad78f6d668430817760fc010773f4ce7d4137
mdnurulislam12 <nurulislam86061907@gmail.com>
2025-06-02 14:14:03 +0600

    dch

--- src/components/Home/Calendar/CHeader/CRightsideBar.tsx
+++ src/components/Home/Calendar/CHeader/CRightsideBar.tsx
@@ -1,14 +1,22 @@
 "use client";
-// import dayjs from "dayjs";
-import React, { useState } from "react";
+import { getWeeks } from "@/lib/getTime";
+import { useDateStore } from "@/lib/storeC";
+import dayjs from "dayjs";
+import React, { Fragment, useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { LuUsers } from "react-icons/lu";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 const CRightsideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { setMonth, selectedMonthIndex, twoDMonthArray } = useDateStore();
   const handleToggleMyCalendar = () => {
     setIsOpen(!isOpen);
   };
+
+  const weekOfMonth = getWeeks(selectedMonthIndex);
+
+  console.log("week of Month===", weekOfMonth);
 
   return (
     <div className="flex flex-col gap-4">
@@ -19,9 +27,67 @@ const CRightsideBar = () => {
         </button>
       </div>
       {/* calender picker  */}
-      <div className="my-6 p-2">
-        <div>
-          <h4></h4>
+      <div className="">
+        <div className="flex items-center justify-between">
+          <div
+            onClick={() => setMonth(selectedMonthIndex - 1)}
+            className="w-6 h-6 bg-gray-300 rounded-full flex items-center justify-center text-black hover:bg-gray-500 hover:text-white cursor-pointer"
+          >
+            <IoIosArrowBack size={15} />
+          </div>
+          <h4>
+            {dayjs(new Date(dayjs().year(), selectedMonthIndex)).format(
+              "MMMM YYYY"
+            )}
+          </h4>
+
+          <div
+            onClick={() => setMonth(selectedMonthIndex + 1)}
+            className="w-6 h-6 bg-gray-300 rounded-full flex items-center justify-center text-black hover:bg-gray-500 hover:text-white cursor-pointer"
+          >
+            <IoIosArrowForward size={15} />{" "}
+          </div>
+        </div>
+        {/* header Row: Days of the week  */}
+        <div className="mt-2 grid grid-cols-[auto_1fr]">
+          <div className="w-full"></div>
+          <div className="grid grid-cols-7 text-xs font-bold">
+            {["S", "M", "T", "W", "T", "F", "S"].map((day, i) => (
+              <span key={i} className="py-1 text-center">
+                {day}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        {/* main content: weeks and days  */}
+        {/* <div className="mt-2 grid grid-cols-[auto_1fr] text-xs">
+          
+          <div className="grid w-6 grid-rows-5 gap-1 gap-y-3 rounded-sm bg-gray-100 p-1">
+            {weekOfMonth.map((week, i)=>(
+              <span key={i}>{week}</span>
+            ))}
+          </div>
+        </div> */}
+
+        {/* dates grid  */}
+        <div className="grid grid-rows-5 grid-cols-7 gap-1 gap-y-3 text-xs mt-2">
+          {twoDMonthArray.map((row, i) => (
+            <Fragment key={i}>
+              {row.map((date, inx) => (
+                <button
+                  key={inx}
+                  className={`${
+                    date.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+                      ? "w-5 h-5 bg-[#463f59] text-white dark:text-black rounded-full flex items-center justify-center p-2"
+                      : ""
+                  } cursor-pointer`}
+                >
+                  <span className="text-center">{date.format("D")}</span>
+                </button>
+              ))}
+            </Fragment>
+          ))}
         </div>
       </div>
       {/* search bar  */}
@@ -41,21 +107,45 @@ const CRightsideBar = () => {
             onClick={handleToggleMyCalendar}
           >
             <span>My Calendar</span>
-            <MdKeyboardArrowDown size={16} className={`${isOpen ? "" : "rotate-180"}`} />
+            <MdKeyboardArrowDown
+              size={16}
+              className={`${isOpen ? "" : "rotate-180"}`}
+            />
           </button>
           {isOpen && (
             <div className="top-12 px-2  w-full rounded-sm overflow-hidden">
               <div className="flex items-center gap-2 accent-[#463f59]">
-                <input type="checkbox" name="" id="work" className="cursor-pointer" />
-                <label htmlFor="work" className="cursor-pointer">Work</label>
+                <input
+                  type="checkbox"
+                  name=""
+                  id="work"
+                  className="cursor-pointer"
+                />
+                <label htmlFor="work" className="cursor-pointer">
+                  Work
+                </label>
               </div>
               <div className="flex items-center gap-2 accent-[#463f59]">
-                <input type="checkbox" name="" id="personal" className="cursor-pointer"/>
-                <label htmlFor="personal" className="cursor-pointer">Personal</label>
+                <input
+                  type="checkbox"
+                  name=""
+                  id="personal"
+                  className="cursor-pointer"
+                />
+                <label htmlFor="personal" className="cursor-pointer">
+                  Personal
+                </label>
               </div>
               <div className="flex items-center gap-2 accent-[#463f59]">
-                <input type="checkbox" name="" id="fitness" className="cursor-pointer" />
-                <label htmlFor="fitness" className="cursor-pointer">Fitness</label>
+                <input
+                  type="checkbox"
+                  name=""
+                  id="fitness"
+                  className="cursor-pointer"
+                />
+                <label htmlFor="fitness" className="cursor-pointer">
+                  Fitness
+                </label>
               </div>
             </div>
           )}
